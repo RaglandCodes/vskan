266010c872e50db00fd4cc951f4e2751517971c2
Ragland Asir <raglandasir@gmail.com>
2024-12-14 07:55:00 +0530

    Header checks

--- mitm-addon/count.py
+++ mitm-addon/count.py
@@ -11,8 +11,17 @@ from typing import Literal
 import logging
 
 
-skan_mode = 'watch' # 'probe' | 'attack'
-target_sites_substring = set()
+target_sites = [
+    # !! First matching rule will apply
+    {
+        'url_regex': '*',
+        'scan_mode': 'watch' # 'probe' | 'attack'
+    }
+]
+
+
+# Active checks 
+# https://docs.gitlab.com/ee/user/application_security/dast/browser/checks/#active-checks
 
 scan_results = {
     'csp': [],
@@ -64,6 +73,7 @@ def check_response_headers_csp(flow):
 
 
 def check_cookie_http_only(flow: http.HTTPFlow):
+    # TODO read: https://docs.gitlab.com/ee/user/application_security/dast/browser/checks/352.1.html
     cookies_in_response = flow.response.cookies
 
     if(len(cookies_in_response) > 0):
@@ -106,12 +116,68 @@ def check_cookie_http_only(flow: http.HTTPFlow):
                     logging.warn("%s is not set HttpOnly.." % x)
                 
 
-                
-                    
+def simple_header_set_check(flow):
+
+    x_content_type_options_header_value = flow.response.headers.get("X-Content-Type-Options") 
+
+    if not x_content_type_options_header_value:
+        logging.warn("X-Content-Type-Options is not set")
+
+    if x_content_type_options_header_value != "nosniff":
+        #TODO: read the multiple stack answers: https://stackoverflow.com/questions/18337630/what-is-x-content-type-options-nosniff
+        # TODO: read https://en.wikipedia.org/wiki/Content_sniffing
+        # TODO: read about mime types: https://developer.mozilla.org/en-US/docs/Web/HTTP/MIME_types#structure_of_a_mime_type
+        logging.warn("X-Content-Type-Options exists but not set to nosniff")
 
 
+
+def simple_header_info_leak_check(flow):
+    x_powered_by_header_value = flow.response.headers.get("X-Powered-By") 
+    x_backend_server_header_value = flow.response.headers.get("X-Backend-Server") 
+
+    if x_powered_by_header_value:
+        # TODO read more: https://www.zaproxy.org/docs/alerts/10037/
+        logging.warn("X-Powered-By is not suppressed")
+
+
+    if x_backend_server_header_value:
+        logging.warn("X-Backend-Server is not suppressed")
+
+    if flow.response.headers.get("X-AspNet-Version"):
+        logging.warn("X-Backend-Server is not suppressed")
+
+
+
+def check_cors(flow, skan_mode):
+
+    #TODO read header forbidden: https://developer.mozilla.org/en-US/docs/Glossary/Forbidden_header_name
+
+    access_control_header = flow.response.headers.get("Access-Control-Allow-Origin")
+
+
+
+    if not access_control_header:
+        logging.warn("Access control header not set")
+        return
+    
+    if access_control_header.strip() == '*':
+        logging.warn("Access control header is allowing all")
 
     
+    if skan_mode == 'probe':
+        pass
+        # TODO check for server generated custom header allowing
+        # https://portswigger.net/web-security/cors
+
+
+
+
+def check_sensitive_in_get(flow):
+    #TODO: read https://docs.gitlab.com/ee/user/application_security/dast/browser/checks/598.3.html
+    pass
+
+
+
 
 
 class Counter:
@@ -128,8 +194,10 @@ class Counter:
         #logging.info("Flow headers %s" % flow.request.headers)
 
     def response(self, flow):
-        check_response_headers_csp(flow)
+        # check_response_headers_csp(flow)
         # check_cookie_http_only(flow)
+
+        check_cors(flow, 'watch')
 
 
 addons = [Counter()]
